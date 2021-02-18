94f25659694426c514444ca0425e25a93596da79
Abdulhakeem Dahir <somlutions@gmail.com>
2021-02-17 16:30:16 -0800

    create transaction with db, access db and add to the db

--- public/db.js
+++ public/db.js
@@ -24,3 +24,16 @@ request.onsuccess = function (event) {
 request.onerror = function (event) {
 	console.log("Woops! " + event.target.errorCode);
 };
+
+// create transaction with db, access db and add to the db
+function saveRecord(record) {
+	const transaction = db.transaction(["pending"], "readwrite");
+	const pendingStore = transaction.objectStore("pending");
+	console.log("Let's see");
+	console.log(record);
+	pendingStore.add({
+		name: record.name,
+		value: record.value,
+		date: record.date,
+	});
+}
