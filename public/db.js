b9aaf2c8398b96158c60a6da7fa2dbf00cf206d3
Abdulhakeem Dahir <somlutions@gmail.com>
2021-02-17 16:27:22 -0800

    created offlineStore

--- public/db.js
+++ public/db.js
@@ -3,3 +3,10 @@ let db;
 //Create a new indexDB request
 const request = indexedDB.open("budget", 1);
 
+request.onupgradeneeded = function (event) {
+    //create an object store called "storeOffline"
+    const db = event.target.result;
+		const offlineStore = db.createObjectStore("offline", {
+			autoIncrement: true,
+		});
+}
